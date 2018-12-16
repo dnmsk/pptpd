60b117c6f28237679421b0d6a63ec60eeb2ce8c3
Dmitry Klimanov <dmitry.klimanov@gmail.com>
2018-12-16 22:20:14 +0300

    Add MTU&MRU options

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,5 @@ default[:pptpd][:localip]  = "10.0.0.1"
 default[:pptpd][:remoteip] = "10.0.0.100-200"
 default[:pptpd][:users] = [{username: "user",password: "password"}]
 default[:pptpd][:dns] = ["8.8.8.8","8.8.4.4"]
+default[:pptpd][:mtu] = "1460"
+default[:pptpd][:mru] = "1460"
